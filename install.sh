b442b098908b83fe75127dbc0c5dfee84b744d26
liberodark <liberodark@gmail.com>
2019-03-18 11:17:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 #=================================================
 
   update_source="https://raw.githubusercontent.com/liberodark/nrpe-installer/master/install.sh"
-  version="0.4.2"
+  version="0.4.3"
 
   echo "Welcome on NRPE Install Script $version"
 
