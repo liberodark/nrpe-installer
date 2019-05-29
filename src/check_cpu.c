6decfb3696181f61982dc76c151ea2f9c2957610
liberodark <liberodark@gmail.com>
2019-05-29 14:18:53 +0200

    go to 0.8.5

--- src/check_cpu.c
+++ src/check_cpu.c
@@ -1,342 +1,146 @@
-/*****************************************************************************
-* 
-* Nagios check_cpu plugin by liberodark & Laurent Levasseur
-* Fork of Nagios check_load plugin
-* 
-* License: GPL
-* Copyright (c) 1999-2014 Nagios Plugins Development Team
-* 
-* Description:
-* 
-* This file contains the check_cpu plugin
-* 
-* This plugin tests the current system load average.
-* 
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* 
-*****************************************************************************/
+#include <ctype.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-const char *progname = "check_cpu";
-const char *copyright = "1999-2019";
-const char *email = "liberodark@gmail.com";
+#include <unistd.h>
 
-#include "common.h"
-#include "utils.h"
-#include "popen.h"
-
-#ifdef HAVE_SYS_LOADAVG_H
-#include <sys/loadavg.h>
-#endif
-
-/* needed for compilation under NetBSD, as suggested by Andy Doran */
-#ifndef LOADAVG_1MIN
-#define LOADAVG_1MIN	0
-#define LOADAVG_5MIN	1
-#define LOADAVG_15MIN	2
-#endif /* !defined LOADAVG_1MIN */
-
-
-static int process_arguments (int argc, char **argv);
-static int validate_arguments (void);
-void print_help (void);
-void print_usage (void);
-
-/* strictly for pretty-print usage in loops */
-static const int nums[3] = { 1, 5, 15 };
-
-/* provide some fairly sane defaults */
-double wload[3] = { 0.0, 0.0, 0.0 };
-double cload[3] = { 0.0, 0.0, 0.0 };
-#define la1 la[0]
-#define la5 la[1]
-#define la15 la[2]
-
-char *status_line;
-int take_into_account_cpus = 0;
-
-static void
-get_threshold(char *arg, double *th)
+struct cpu_stats
 {
-	size_t i, n;
-	int valid = 0;
-	char *str = arg, *p;
+	uint64_t user, nice, system, idle;
+};
 
-	n = strlen(arg);
-	for(i = 0; i < 3; i++) {
-		th[i] = strtod(str, &p);
-		if(p == str) break;
-
-		valid = 1;
-		str = p + 1;
-		if(n <= (size_t)(str - arg)) break;
-	}
-
-	/* empty argument or non-floatish, so warn about it and die */
-	if(!i && !valid) usage (_("Warning threshold must be float or float triplet!\n"));
-
-	if(i != 2) {
-		/* one or more numbers were given, so fill array with last
-		 * we got (most likely to NOT produce the least expected result) */
-		for(n = i; n < 3; n++) th[n] = th[i];
-	}
-}
-
-
-int
-main (int argc, char **argv)
+static unsigned int get_core_count(void)
 {
-	int result;
-	int i;
-	long numcpus;
-
-	double la[3] = { 0.0, 0.0, 0.0 };	/* NetBSD complains about unitialized arrays */
-#ifndef HAVE_GETLOADAVG
-	char input_buffer[MAX_INPUT_BUFFER];
-# ifdef HAVE_PROC_LOADAVG
 	FILE *fp;
-	char *str, *next;
-# endif
-#endif
+	char line[1024];
+	unsigned int tmp, count;
 
-	setlocale (LC_ALL, "");
-	bindtextdomain (PACKAGE, LOCALEDIR);
-	textdomain (PACKAGE);
-	setlocale(LC_NUMERIC, "POSIX");
+	count = 0;
 
-	/* Parse extra opts if any */
-	argv = np_extra_opts (&argc, argv, progname);
+	fp = fopen("/proc/stat", "r");
 
-	if (process_arguments (argc, argv) == ERROR)
-		usage4 (_("Could not parse arguments"));
+	while (!feof(fp) && !ferror(fp))
+	{
+		if (!fgets(line, sizeof(line), fp))
+			continue;
 
-#ifdef HAVE_GETLOADAVG
-	result = getloadavg (la, 3);
-	if (result != 3)
-		return STATE_UNKNOWN;
-#else
-# ifdef HAVE_PROC_LOADAVG
-	fp = fopen (PROC_LOADAVG, "r");
-	if (fp == NULL) {
-		printf (_("Error opening %s\n"), PROC_LOADAVG);
-		return STATE_UNKNOWN;
+		if (strncmp(&line[0], "cpu", 3) != 0)
+			continue;
+
+		if (!isdigit(line[3]))
+			continue;
+
+		tmp = strtoul(&line[3], NULL, 10);
+		if (tmp > count)
+			count = tmp;
 	}
 
-	while (fgets (input_buffer, MAX_INPUT_BUFFER - 1, fp)) {
-		str = (char *)input_buffer;
-		for(i = 0; i < 3; i++) {
-			la[i] = strtod(str, &next);
-			str = next;
-		}
-	}
+	fclose(fp);
 
-	fclose (fp);
-# else
-	child_process = spopen (PATH_TO_UPTIME);
-	if (child_process == NULL) {
-		printf (_("Error opening %s\n"), PATH_TO_UPTIME);
-		return STATE_UNKNOWN;
-	}
-	child_stderr = fdopen (child_stderr_array[fileno (child_process)], "r");
-	if (child_stderr == NULL) {
-		printf (_("Could not open stderr for %s\n"), PATH_TO_UPTIME);
-	}
-	fgets (input_buffer, MAX_INPUT_BUFFER - 1, child_process);
-    if(strstr(input_buffer, "load average:")) {
-	    sscanf (input_buffer, "%*[^l]load average: %lf, %lf, %lf", &la1, &la5, &la15);
-    }
-    else if(strstr(input_buffer, "load averages:")) {
-	    sscanf (input_buffer, "%*[^l]load averages: %lf, %lf, %lf", &la1, &la5, &la15);
-    }
-    else {
-		printf (_("could not parse load from uptime: %s\n"), result, PATH_TO_UPTIME);
-		return STATE_UNKNOWN;
-    }
-
-	result = spclose (child_process);
-	if (result) {
-		printf (_("Error code %d returned in %s\n"), result, PATH_TO_UPTIME);
-		return STATE_UNKNOWN;
-	}
-# endif
-#endif
-
-	if (take_into_account_cpus == 1) {
-		if ((numcpus = GET_NUMBER_OF_CPUS()) > 0) {
-			la[0] = la[0] / numcpus;
-			la[1] = la[1] / numcpus;
-			la[2] = la[2] / numcpus;
-		}
-	}
-	if ((la[0] < 0.0) || (la[1] < 0.0) || (la[2] < 0.0)) {
-#ifdef HAVE_GETLOADAVG
-		printf (_("Error in getloadavg()\n"));
-#else
-# ifdef HAVE_PROC_LOADAVG
-		printf (_("Error processing %s\n"), PROC_LOADAVG);
-# else
-		printf (_("Error processing %s\n"), PATH_TO_UPTIME);
-# endif
-#endif
-		return STATE_UNKNOWN;
-	}
-
-	/* we got this far, so assume OK until we've measured */
-	result = STATE_OK;
-	
-	if (take_into_account_cpus == 1 && (numcpus = GET_NUMBER_OF_CPUS()) > 1){
-
-	xasprintf(&status_line, _("load average per CPU: %.2f, %.2f, %.2f"), la1, la5, la15);
-	}
-	else
-	xasprintf(&status_line, _("load average: %.2f, %.2f, %.2f"), la1, la5, la15);
-
-	for(i = 0; i < 3; i++) {
-		if(la[i] > cload[i]) {
-			result = STATE_CRITICAL;
-			break;
-		}
-		else if(la[i] > wload[i]) result = STATE_WARNING;
-	}
-
-	printf("%s - %s|", state_text(result), status_line);
-	for(i = 0; i < 3; i++)
-		printf("load%d=%.3f;%.3f;%.3f;0; ", nums[i], la[i], wload[i], cload[i]);
-
-	putchar('\n');
-	return result;
+	return count + 1;
 }
 
-
-/* process command-line arguments */
-static int
-process_arguments (int argc, char **argv)
+static void dump_cpu(struct cpu_stats *cpu, const char *s)
 {
-	int c = 0;
+	sscanf(s, "%" SCNu64 " %" SCNu64 " %" SCNu64 " %" SCNu64,
+			&cpu->user, &cpu->nice, &cpu->system, &cpu->idle);
+}
 
-	int option = 0;
-	static struct option longopts[] = {
-		{"warning", required_argument, 0, 'w'},
-		{"critical", required_argument, 0, 'c'},
-		{"percpu", no_argument, 0, 'r'},
-		{"version", no_argument, 0, 'V'},
-		{"help", no_argument, 0, 'h'},
-		{0, 0, 0, 0}
-	};
+static void dump_cores(struct cpu_stats *cpu, unsigned int count, struct cpu_stats *cores)
+{
+	FILE *fp;
+	char line[1024];
+	unsigned int i;
+	char *nptr;
 
-	if (argc < 2)
-		return ERROR;
+	fp = fopen("/proc/stat", "r");
 
-	while (1) {
-		c = getopt_long (argc, argv, "Vhrc:w:", longopts, &option);
+	while (!feof(fp) && !ferror(fp))
+	{
+		if (!fgets(line, sizeof(line), fp))
+			continue;
 
-		if (c == -1 || c == EOF)
-			break;
+		if (strncmp(&line[0], "cpu", 3) != 0)
+			continue;
 
-		switch (c) {
-		case 'w': /* warning time threshold */
-			get_threshold(optarg, wload);
-			break;
-		case 'c': /* critical time threshold */
-			get_threshold(optarg, cload);
-			break;
-		case 'r': /* Divide load average by number of CPUs */
-			take_into_account_cpus = 1;
-			break;
-		case 'V':									/* version */
-			print_revision (progname, NP_VERSION);
-			exit (STATE_OK);
-		case 'h':									/* help */
-			print_help ();
-			exit (STATE_OK);
-		case '?':									/* help */
-			usage5 ();
+		if (isspace(line[3]))
+		{
+			dump_cpu(cpu, &line[4]);
+			continue;
 		}
+
+		if (!isdigit(line[3]))
+			continue;
+
+		i = strtoul(&line[3], &nptr, 10);
+		if (i >= count)
+			continue;
+
+		dump_cpu(&cores[i], nptr);
 	}
 
-	c = optind;
-	if (c == argc)
-		return validate_arguments ();
-
-	/* handle the case if both arguments are missing,
-	 * but not if only one is given without -c or -w flag */
-	if(c - argc == 2) {
-		get_threshold(argv[c++], wload);
-		get_threshold(argv[c++], cload);
-	}
-	else if(c - argc == 1) {
-		get_threshold(argv[c++], cload);
-	}
-
-	return validate_arguments ();
+	fclose(fp);
 }
 
-
-
-static int
-validate_arguments (void)
+static float get_cpu_pct(struct cpu_stats *cpu0, struct cpu_stats *cpu1)
 {
-	int i = 0;
+	uint64_t used_total0 = cpu0->user + cpu0->nice + cpu0->system;
+	uint64_t used_total1 = cpu1->user + cpu1->nice + cpu1->system;
+	uint64_t total0 = used_total0 + cpu0->idle;
+	uint64_t total1 = used_total1 + cpu1->idle;
 
-	/* match cload first, as it will give the most friendly error message
-	 * if user hasn't given the -c switch properly */
-	for(i = 0; i < 3; i++) {
-		if(cload[i] < 0)
-			die (STATE_UNKNOWN, _("Critical threshold for %d-minute load average is not specified\n"), nums[i]);
-		if(wload[i] < 0)
-			die (STATE_UNKNOWN, _("Warning threshold for %d-minute load average is not specified\n"), nums[i]);
-		if(wload[i] > cload[i])
-			die (STATE_UNKNOWN, _("Parameter inconsistency: %d-minute \"warning load\" is greater than \"critical load\"\n"), nums[i]);
+	return (used_total1 - used_total0) * 10000 / (total1 - total0) / 100.f;
+}
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	float warn_threshold;
+	float crit_threshold;
+	unsigned int core_count;
+	struct cpu_stats cpu0, cpu1;
+	struct cpu_stats *cores0, *cores1;
+	float cpu_pct;
+	const char *panic_str;
+
+	/* cmd line : $0 -w warn_threshold -c crit_threshold */
+	warn_threshold = strtof(argv[2], NULL);
+	crit_threshold = strtof(argv[4], NULL);
+
+	core_count = get_core_count();
+	if (core_count)
+	{
+		cores0 = calloc(core_count, sizeof(cores0[0]));
+		cores1 = calloc(core_count, sizeof(cores1[0]));
 	}
 
-	return OK;
-}
+	dump_cores(&cpu0, core_count, cores0);
 
+	sleep(1);
 
+	dump_cores(&cpu1, core_count, cores1);
 
-void
-print_help (void)
-{
-	print_revision (progname, NP_VERSION);
+	cpu_pct = get_cpu_pct(&cpu0, &cpu1);
 
-	printf ("Copyright (c) 1999 Felipe Gustavo de Almeida <galmeida@linux.ime.usp.br>\n");
-	printf (COPYRIGHT, copyright, email);
+	panic_str = "OK";
+	if (cpu_pct > crit_threshold)
+		panic_str = "Critical";
+	else if (cpu_pct > warn_threshold)
+		panic_str = "Warning";
 
-	printf (_("This plugin tests the current system load average."));
+	printf("%s: CPU Used = %.2f%% | ", panic_str, cpu_pct);
 
-  printf ("\n\n");
+	for (unsigned int i = 0; i < core_count; i++)
+		printf("Core %u = %.2f%%%s", i, get_cpu_pct(&cores0[i], &cores1[i]), (i < core_count - 1) ? "; " : "\n");
 
-	print_usage ();
+	if (core_count)
+	{
+		free(cores0);
+		free(cores1);
+	}
 
-	printf (UT_HELP_VRSN);
-	printf (UT_EXTRA_OPTS);
-
-	printf (" %s\n", "-w, --warning=WLOAD1,WLOAD5,WLOAD15");
-  printf ("    %s\n", _("Exit with WARNING status if load average exceeds WLOADn"));
-  printf (" %s\n", "-c, --critical=CLOAD1,CLOAD5,CLOAD15");
-  printf ("    %s\n", _("Exit with CRITICAL status if load average exceed CLOADn"));
-  printf ("    %s\n", _("the load average format is the same used by \"uptime\" and \"w\""));
-  printf (" %s\n", "-r, --percpu");
-  printf ("    %s\n", _("Divide the load averages by the number of CPUs (when possible)"));
-
-	printf (UT_SUPPORT);
-}
-
-void
-print_usage (void)
-{
-  printf ("%s\n", _("Usage:"));
-	printf ("%s [-r] -w WLOAD1,WLOAD5,WLOAD15 -c CLOAD1,CLOAD5,CLOAD15\n", progname);
+	return 0;
 }
