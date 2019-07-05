4866cabde69abdfb776623b70d62267eac73e04a
liberodark <liberodark@gmail.com>
2019-07-05 10:57:59 +0200

    fix graph

--- src/check_cpu.c
+++ src/check_cpu.c
@@ -138,10 +138,12 @@ int main(int argc, char **argv)
 		result = 1;
 	}
 
-	printf("CPU %s - Used = %.2f%% | ", panic_str, cpu_pct);
+	printf("CPU %s - Used = %.2f%% |", panic_str, cpu_pct);
 
 	for (unsigned int i = 0; i < core_count; i++)
-		printf("Core %u = %.2f%%%s", i, get_cpu_pct(&cores0[i], &cores1[i]), (i < core_count - 1) ? "; " : "\n");
+		printf("'Core %u'=%.2f%%;%s", i, get_cpu_pct(&cores0[i], &cores1[i]), (i < core_count - 1) ? " " : "");
+
+	printf("\n");
 
 	if (core_count)
 	{
