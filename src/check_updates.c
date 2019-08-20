014cf669e8a1a3bf3943cec32bb09b00e50a80d5
liberodark <liberodark@gmail.com>
2019-08-20 11:37:17 +0200

    update

--- src/check_updates.c
+++ src/check_updates.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <glib.h>
 #include <gmodule.h>
 
 #define I_KNOW_THE_PACKAGEKIT_GLIB2_API_IS_SUBJECT_TO_CHANGE 1
@@ -166,8 +167,20 @@ const char *get_opt_str(struct opt *opts, enum opt_id id)
 
 int print_usage(int argc, char **argv)
 {
-	fprintf(stderr, "Usage: %s -w <warn_treshold> -c <crit_treshold> [-security-update] [-update]\n", (argc >= 1) ? argv[0] : "check_pkg");
+	fprintf(stderr, "Usage: %s [-w <warn_treshold>] [-c <crit_treshold>] [-security-update] [-update]\n", (argc >= 1) ? argv[0] : "check_pkg");
 	return 1;
+}
+
+static gint pkg_cmp_by_id(gconstpointer a, gconstpointer b)
+{
+	return strcmp(pk_package_get_id(*(PkPackage **)a),
+			pk_package_get_id(*(PkPackage **)b));
+}
+
+static gint upd_cmp_by_pkg_id(gconstpointer a, gconstpointer b)
+{
+	return strcmp(pk_update_detail_get_package_id(*(PkUpdateDetail **)a),
+			pk_update_detail_get_package_id(*(PkUpdateDetail **)b));
 }
 
 int main(int argc, char **argv)
@@ -178,9 +191,9 @@ int main(int argc, char **argv)
 #define OPT_END() [OPT_COUNT] = { .type = OPT_TYPE_INVAL, .name = NULL }
 	struct opt opt_tpl[] = {
 		OPT_INT(OPT_C, "-c"),
-		OPT_INT(OPT_W, "-w"),
 		OPT_NONE(OPT_SECURITY_UPDATE, "-security-update"),
 		OPT_NONE(OPT_UPDATE, "-update"),
+		OPT_INT(OPT_W, "-w"),
 		OPT_END()
 	};
 #undef OPT_END
@@ -191,13 +204,16 @@ int main(int argc, char **argv)
 	const char *reason = "N/A";
 	int result = -1;
 	struct opt *opts;
+	int warn_treshold = INT_MAX;
+	int crit_treshold = INT_MAX;
 	int must_upd_pkg = 0;
 	int must_upd_sec_pkg = 0;
 	PkClient *cli;
 	GError *gerror = NULL;
 	PkResults *pk_results;
 	GPtrArray *pkgs;
-	GPtrArray *upd_pkg_ids;
+	GPtrArray *pkg_ids;
+	GPtrArray *upds;
 	size_t total_upd_count = 0;
 	size_t sec_upd_count = 0;
 	const char *panic_str;
@@ -206,9 +222,10 @@ int main(int argc, char **argv)
 	if (!opts)
 		return print_usage(argc, argv);
 
-	if (!get_opt_defined(opts, OPT_C) || !get_opt_defined(opts, OPT_W))
-		return print_usage(argc, argv);
-
+	if (get_opt_defined(opts, OPT_W))
+		warn_treshold = get_opt_int(opts, OPT_W);
+	if (get_opt_defined(opts, OPT_C))
+		crit_treshold = get_opt_int(opts, OPT_C);
 	must_upd_pkg = get_opt_defined(opts, OPT_UPDATE);
 	must_upd_sec_pkg = get_opt_defined(opts, OPT_SECURITY_UPDATE);
 
@@ -222,7 +239,7 @@ int main(int argc, char **argv)
 	pk_results = pk_client_get_updates(cli, PK_FILTER_ENUM_NONE, NULL, NULL, NULL, &gerror);
 	if (!pk_results)
 	{
-		reason = "Failed to get the list of updates";
+		reason = "Failed to get the update list";
 		goto fail;
 	}
 
@@ -230,49 +247,88 @@ int main(int argc, char **argv)
 	g_object_unref(pk_results);
 	if (!pkgs)
 	{
-		reason = "Failed to get packages from the list of updates";
+		reason = "Failed to get packages from the update list";
 		goto fail;
 	}
 
+	if (pkgs->len == 0)
+	{
+		/* Short path */
+		g_ptr_array_unref(pkgs);
+		goto done;
+	}
+
+	g_ptr_array_sort(pkgs, pkg_cmp_by_id);
+
 	total_upd_count = pkgs->len;
 
-	if (must_upd_pkg || must_upd_sec_pkg)
-	{
-		/* Keep a ref for pkg ids */
-		g_ptr_array_ref(pkgs);
-		upd_pkg_ids = g_ptr_array_sized_new(total_upd_count + 1);
-	}
+	pkg_ids = g_ptr_array_sized_new(total_upd_count + 1);
 
 	for (size_t i = 0; i < pkgs->len; i++)
 	{
 		PkPackage *pkg = (PkPackage *)g_ptr_array_index(pkgs, i);
-		PkInfoEnum pkg_info;
+		const gchar *pkg_id = pk_package_get_id(pkg);
+
+		g_ptr_array_add(pkg_ids, (gchar *)pkg_id);
+	}
+
+	g_ptr_array_add(pkg_ids, NULL);
+
+	pk_results = pk_client_get_update_detail(cli, (gchar **)pkg_ids->pdata, NULL, NULL, NULL, &gerror);
+	g_ptr_array_unref(pkg_ids);
+//	g_ptr_array_unref(pkgs);
+	if (!pk_results)
+	{
+		reason = "Failed to get packages update detail";
+		goto fail;
+	}
+
+	upds = pk_results_get_update_detail_array(pk_results);
+	g_object_unref(pk_results);
+	if (!upds)
+	{
+		reason = "Failed to get packages update detail";
+		goto fail;
+	}
+
+	g_assert_true(upds->len == pkgs->len);
+
+	g_ptr_array_sort(upds, upd_cmp_by_pkg_id);
+
+	pkg_ids = g_ptr_array_sized_new(total_upd_count + 1);
+
+	for (size_t i = 0; i < upds->len; i++)
+	{
+		PkUpdateDetail *upd = (PkUpdateDetail *)g_ptr_array_index(upds, i);
+		PkPackage *pkg = (PkPackage *)g_ptr_array_index(pkgs, i);
+		const gchar *pkg_id = pk_update_detail_get_package_id(upd);
+		gchar **cve_urls = pk_update_detail_get_cve_urls(upd);
 		int add_pkg = 0;
+
+		g_assert_cmpstr(pkg_id, ==, pk_package_get_id(pkg));
 
 		if (must_upd_pkg)
 			add_pkg = 1;
 
-		pkg_info = pk_package_get_info(pkg);
-		if (pkg_info & PK_INFO_ENUM_SECURITY)
+		if (cve_urls && cve_urls[0])
 		{
 			sec_upd_count++;
+
 			if (must_upd_sec_pkg)
 				add_pkg = 1;
 		}
 
 		if (add_pkg)
-			g_ptr_array_add(upd_pkg_ids, (void *)pk_package_get_id(pkg));
+			g_ptr_array_add(pkg_ids, (gchar *)pkg_id);
 	}
-
-	g_ptr_array_unref(pkgs);
 
 	if (must_upd_pkg || must_upd_sec_pkg)
 	{
-		g_ptr_array_add(upd_pkg_ids, NULL);
+		g_ptr_array_add(pkg_ids, NULL);
 
-		pk_results = pk_client_update_packages(cli, 0, (gchar **)upd_pkg_ids->pdata, NULL, NULL, NULL, &gerror);
-		g_ptr_array_unref(upd_pkg_ids);
-		g_ptr_array_unref(pkgs);
+		pk_results = pk_client_update_packages(cli, 0, (gchar **)pkg_ids->pdata, NULL, NULL, NULL, &gerror);
+		g_ptr_array_unref(pkg_ids);
+		g_ptr_array_unref(upds);
 		if (!pk_results)
 		{
 			reason = "Failed to update packages";
@@ -280,14 +336,16 @@ int main(int argc, char **argv)
 		}
 	}
 
+
+done:
 	panic_str = "OK";
 	status = 0;
-	if (sec_upd_count >= get_opt_int(opts, OPT_C))
+	if (sec_upd_count >= crit_treshold)
 	{
 		panic_str = "Critical";
 		status = 2;
 	}
-	else if (sec_upd_count >= get_opt_int(opts, OPT_W))
+	else if (sec_upd_count >= warn_treshold)
 	{
 		panic_str = "Warning";
 		status = 1;
