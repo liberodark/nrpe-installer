a9d4081da6afb2fc5241e17f1f3ddc77d8824097
liberodark <liberodark@gmail.com>
2023-05-25 13:55:52 +0200

    Update backup_rsyslog.sh

--- src/backup_rsyslog.sh
+++ src/backup_rsyslog.sh
@@ -5,8 +5,14 @@
 # Thanks : 
 # License: GNU GPLv3
 
-version="0.0.2"
+version="0.0.3"
 echo "Welcome on Backup Log Script $version"
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 #=================================================
 # RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
@@ -21,17 +27,24 @@ PWD="My_Password"
 bin_check(){
 if ! command -v check_logs > /dev/null 2>&1; then
     echo "check_logs not installed"
-    exit
+    ln -s /usr/local/nagios/libexec/check_logs /usr/bin/check_logs || exit
 fi
 }
 
 move_log(){
-mount -a || echo "Mount NFS Error" && exit
+if ! mount -a; then
+    echo "Mount NFS Error"
+    export DIR_365="/Backup-Local/365/"
+    export DIR_90="/Backup-Local/90/"
+    mkdir -p "${DIR_365}" || exit
+    mkdir -p "${DIR_90}" || exit
+fi
+
 mkdir -p "${TEMP_365}" || echo "Create ${TEMP_365} Error" && exit
 mkdir -p "${TEMP_90}" || echo "Create ${TEMP_90} Error" && exit
-mv /Data/365/*.log "${TEMP_365}" || echo "Move Log in ${TEMP_365} Error" && exit
-mv /Data/90/*.log "${TEMP_90}" || echo "Move Log in ${TEMP_90} Error" && exit
-systemctl restart rsyslog || echo "Rsyslog Service Error" && exit
+mv /Data/365/*.log "${TEMP_365}" || echo "Move Log in ${TEMP_365} Error"
+mv /Data/90/*.log "${TEMP_90}" || echo "Move Log in ${TEMP_90} Error"
+systemctl restart rsyslog || echo "Rsyslog Service Error"
 }
 
 backup_365(){
